e77a91f31966a679ec0d2b16d48a4ebebe7cb956
Aman Kumar <authorisedaman@gmail.com>
2024-07-17 11:20:53 +0530

    added

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { auth, firestore } from "../config/firebase";
 import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
@@ -12,13 +13,16 @@ const AuthProvider = ({ children }) => {
       .then(() => {
         console.log("Sign-out successful.");
         setUserData(false);
+        window.location.reload();
       })
       .catch((error) => {
         console.error("Error signing out:", error);
       });
   };
 
-  const [favno, setFavNo] = useState( userData && userData.favourites.length || 0);
+  const [favno, setFavNo] = useState(
+    (userData && userData.favourites.length) || 0
+  );
 
   return (
     <AuthContext.Provider
